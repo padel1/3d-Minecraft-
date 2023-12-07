0a073fc8ba1992ccdcf1d9bcc655622134cb18ac
desi6ner <semaraademia@gmail.com>
2023-12-07 01:29:41 +0100

    multiplayer 2 ta3 sba7

--- scene.py
+++ scene.py
@@ -7,6 +7,8 @@ from cube import *
 from camera import *
 from texture import *
 from help import *
+from network import Network
+from player import Player
 
 
 class Scene:
@@ -24,7 +26,7 @@ class Scene:
         self.camera = Camera()
         self.clock = pygame.time.Clock()
         # cubes
-        self.cubes = [Cube((78, -150, -15), 0.3, textures4["sand"]),]
+        self.cubes = []
 
         # move
         self.i_walk = 0
@@ -50,6 +52,10 @@ class Scene:
         self.hovered_face = None
         self.sorted_cubes = None
         self.hoverd_cube = None
+        self.sun = Cube((78, -150, -15), 0.3, Texture.sand)
+        self.other_palyer = None
+        self.other_palyer_rotation_h = None
+        self.other_palyer_rotation_v = None
 
     def handle_input(self):
 
@@ -246,21 +252,19 @@ class Scene:
             new_position[1] += 1
             if not self.check_collision(new_position):
                 self.camera.position = new_position
-       
 
         if self.camera.position[1] < -3:
             self.acceleration += 0.1
-            
-            
 
             if not self.check_collision(self.camera.position+np.array([0, self.acceleration, 0])):
-                
-                self.camera.position[1] = self.camera.position[1] + self.acceleration
+
+                self.camera.position[1] = self.camera.position[1] + \
+                    self.acceleration
             else:
                 self.acceleration = 0.2
-                
+
         else:
-            self.camera.position[1]= -3
+            # self.camera.position[1] = -3
             self.acceleration = 0.2
 
     def check_collision(self, new_position):
@@ -392,8 +396,7 @@ class Scene:
         rotate_h = rotate_matrix_y(self.camera.angle_h)
         rotate_v = rotate_matrix_x(self.camera.angle_v)
 
-        sun = self.cubes[0]
-        sun_center = sun.points[0]
+        sun_center = self.sun.points[0]
         sun_center[0] += self.x
         sun_center = sun_center - self.camera.position
         sun_center = np.dot(sun_center, rotate_h)
@@ -413,34 +416,83 @@ class Scene:
             transformed_points = transform_points(
                 transformed_points, self.camera.f)
             points_2d = np.dot(self.camera.K, transformed_points.T).T
+
             at_least_one_point_in_view = any(
                 0 <= point[0] < screen_width and 0 <= point[1] < screen_height for point in points_2d
             )
+
             for surface in surfaces[3:]:
-                # check if collid
 
                 if at_least_one_point_in_view and all(
-                    point[-1] > self.camera.f for _, point in surface
+                    point[-1] > self.camera.f for _,  point in surface[1]
                 ):
-                    pts = [points_2d[point] for point, _ in surface]
-                    ptsd = [point for point, _ in surface]
+                    pts = []
+                    # lwjah = surface[0]
+                    for point, _ in surface[1]:
+                        pts.append(points_2d[point])
 
-                    surface_points = np.array([a for _, a in surface])
+                    ptsd = [point for point, _ in surface[1]]
+
+                    surface_points = np.array([a for _, a in surface[1]])
 
                     intensity = simulate_sunlight(surface_points, sun_center)
 
-                    if cube == self.cubes[0]:
-                        intensity = 1
                     if cube == self.hoverd_cube:
                         if np.array_equal(np.sort(ptsd), np.sort(self.hovered_face)):
                             draw_polygon(self.screen, pts,
-                                         cube.texture, 1, True)
+                                         textures8[cube.texture], 1, True)
                             self.hovered_face = []
                         else:
                             draw_polygon(self.screen, pts,
-                                         cube.texture, intensity)
+                                         textures8[cube.texture], intensity)
                     else:
-                        draw_polygon(self.screen, pts, cube.texture, intensity)
+                        draw_polygon(self.screen, pts,
+                                     textures8[cube.texture], intensity)
+
+        # draw other player player
+        if self.other_palyer != None:
+            self.other_palyer_rotation_h += ROTATION_SPEED
+            self.other_palyer_rotation_v += ROTATION_SPEED
+
+            points = self.other_palyer.points - self.other_palyer.center
+            points = np.dot(points, rotate_matrix_y(
+                self.other_palyer_rotation_h))
+            points = np.dot(points, rotate_matrix_x(
+                self.other_palyer_rotation_v))
+            points = points + self.other_palyer.center
+            new_points = points - self.camera.position
+
+            transformed_points = np.dot(new_points, rotate_h)
+            transformed_points = np.dot(transformed_points, rotate_v)
+
+            surfaces = get_surfaces(transformed_points)
+
+            transformed_points = transform_points(
+                transformed_points, self.camera.f)
+            points_2d = np.dot(self.camera.K, transformed_points.T).T
+
+            at_least_one_point_in_view = any(
+                0 <= point[0] < screen_width and 0 <= point[1] < screen_height for point in points_2d
+            )
+
+            for surface in surfaces[3:]:
+
+                if at_least_one_point_in_view and all(
+                    point[-1] > self.camera.f for _,  point in surface[1]
+                ):
+                    pts = []
+                    # lwjah = surface[0]
+                    for point, _ in surface[1]:
+                        pts.append(points_2d[point])
+
+                    ptsd = [point for point, _ in surface[1]]
+
+                    surface_points = np.array([a for _, a in surface[1]])
+
+                    intensity = simulate_sunlight(surface_points, sun_center)
+
+                    draw_polygon(self.screen, pts,
+                                 face_texture, intensity)
 
         self.screen.blit(gui["crosshair"], (half_width -
                          gui["crosshair"].get_width()//2, half_height - gui["crosshair"].get_height()//2))
@@ -453,8 +505,39 @@ class Scene:
 
         bg_idx = 1
         i = 0
+        n = Network()
+        player_id = n.game_info["player_id"]
+        players = n.game_info["players"]
+        cubes = n.game_info["cubes"]
+        self.cubes = cubes
+        self.camera.position = players[player_id].position
+        self.player = Player(
+            players[player_id], players[player_id].rotation_h, players[player_id].rotation_v)
 
         while True:
+
+            players[player_id].position = self.camera.position
+            players[player_id].rotation_v = self.camera.angle_v
+            players[player_id].rotation_h = self.camera.angle_v
+
+            data = n.send(
+                {"player_id": player_id, "players": players, "cubes": self.cubes}
+            )
+
+            players = data["players"]
+            if len(players) > 1:
+                if data["player_id"] == 0:
+                    self.other_palyer = Cube(players[1].position, 1, "")
+                    self.other_palyer_rotation_v = players[1].rotation_v
+                    self.other_palyer_rotation_h = players[1].rotation_h
+                else:
+                    self.other_palyer = Cube(players[0].position, 1, "")
+                    self.other_palyer_rotation_v = players[0].rotation_v
+                    self.other_palyer_rotation_h = players[0].rotation_h
+
+            cubes = data["cubes"]
+
+            self.cubes = cubes
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -500,12 +583,16 @@ class Scene:
                             new_cube_position = intersection_cube.center + \
                                 face_offsets[closest_face_index]
                             # Create a new cube only if the cube will not colide with me
-                            
-                            if math.dist(new_cube_position ,self.camera.position) > CUBE_SIZE +3:
+
+                            if math.dist(new_cube_position, self.camera.position) > CUBE_SIZE + 3:
                                 new_cube = Cube(new_cube_position,
-                                                CUBE_SIZE, textures4["sand"])
-                                new_cube.texture = textures4[list(textures4.keys())[
-                                    self.selected_texture_index]]
+                                                CUBE_SIZE, Texture.sand)
+                                new_cube.texture = list(textures8.keys())[
+                                    self.selected_texture_index]
+                                # if self.selected_texture_index==0:
+                                #     new_cube.texture
+
+                                # Texture.
                                 self.sound.pick.play()
                                 self.cubes.append(new_cube)
                                 self.mouse_button_down = False
@@ -521,10 +608,11 @@ class Scene:
                                     intersection_polygon, axis=0)+np.array([0, -CUBE_SIZE//2, 0])
 
                                 new_cube = Cube(new_cube_position,
-                                                CUBE_SIZE, textures4["sand"])
-                                new_cube.texture = textures4[list(textures4.keys())[
-                                    self.selected_texture_index]]
+                                                CUBE_SIZE, Texture.sand)
+                                new_cube.texture = list(textures8.keys())[
+                                    self.selected_texture_index]
                                 self.sound.pick.play()
+
                                 self.cubes.append(new_cube)
 
                             elif intersection_point is not None:
@@ -532,9 +620,9 @@ class Scene:
                                 new_cube_position[1] -= CUBE_SIZE//2
 
                                 new_cube = Cube(new_cube_position,
-                                                CUBE_SIZE, textures4["sand"])
-                                new_cube.texture = textures4[list(textures4.keys())[
-                                    self.selected_texture_index]]
+                                                CUBE_SIZE, Texture.sand)
+                                new_cube.texture = list(textures8.keys())[
+                                    self.selected_texture_index]
                                 self.sound.pick.play()
                                 self.cubes.append(new_cube)
 
---
+++ server.py
@@ -0,0 +1,103 @@
+import socket
+import pickle
+import threading
+import random
+from cube import Cube
+from player import Player
+import numpy as np
+import random
+# Define server address and port
+SERVER_ADDRESS = ("192.168.1.35", 5555)
+BUFFER_SIZE = 4096 * 4
+
+player_id = 0
+cubes = []
+
+
+players = []
+lock = threading.Lock()
+
+
+def handle_client(client_socket, player_id):
+    global players, cubes
+    players.append(
+        Player(([random.randint(0, 10), -5, random.randint(0, 10)]), 90, 90))
+    client_socket.sendall(
+        pickle.dumps(
+            {"player_id": player_id, "players": players, "cubes": cubes})
+    )
+
+    while True:
+        try:
+            data = client_socket.recv(BUFFER_SIZE)
+            if not data:
+                break
+
+            # Unpickle the received data
+            data = pickle.loads(data)
+
+            p_id = data["player_id"]
+            ps = data["players"]
+            cbs = data["cubes"]
+
+            # Update player position
+            with lock:
+                players[p_id].position = ps[p_id].position
+                players[p_id].rotation_h = ps[p_id].rotation_h
+                players[p_id].rotation_v = ps[p_id].rotation_v
+
+
+            # Check for collision with small squares
+            with lock:
+                for cube in cbs:
+                    if not any(np.array_equal(cube.center, c.center) for c in cubes):
+                        cubes.append(cube)
+
+            # Send updated player information and small squares to all clients
+            with lock:
+                client_socket.sendall(
+                    pickle.dumps(
+                        {"player_id": player_id, "players": players, "cubes": cubes}
+                    )
+                )
+
+        except Exception as e:
+            print(f"Error handling client {player_id}: {e}")
+            break
+
+    print(f"Player {player_id} disconnected.")
+    # Remove the player from the list upon disconnection
+    with lock:
+        del players[player_id]
+
+    client_socket.close()
+
+# Main server loop
+
+
+def main():
+
+    global cubes
+    global players
+    global player_id
+
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind(SERVER_ADDRESS)
+    server_socket.listen()  # Allow up to 3 connections
+    print("Server is listening for connections...")
+
+    while True:
+        client_socket, client_address = server_socket.accept()
+        print(f"Connection established with   {client_address}")
+        print(f"Player {player_id} connected.")
+
+        # Start a new thread to handle the client
+        threading.Thread(target=handle_client, args=(
+            client_socket, player_id)).start()
+
+        # Increment player ID for the next client
+        player_id += 1
+
+
+if __name__ == "__main__":
+    main()
